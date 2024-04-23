36026dc32f4b1b17916fa6f21f414899884143d5
Mari Calmer <superlassig@gmail.com>
2024-04-23 20:55:22 +0200

    feat: add utility file for calendar time zones

---
+++ src/utils/timezones.js
@@ -0,0 +1,8 @@
+export const usTimeZones = [
+  { value: 'America/New_York', label: 'GMT-4' },
+  { value: 'America/Chicago', label: 'GMT-5' },
+  { value: 'America/Denver', label: 'GMT-6' },
+  { value: 'America/Phoenix', label: 'GMT-7' },
+  { value: 'America/Anchorage', label: 'GMT-8' },
+  { value: 'America/Adak', label: 'GMT-9' }
+];
